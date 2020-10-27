6896f217606ec2a038deda0eb0b93fca634dcb3a
Eric B <eric.bezzam@epfl.ch>
2020-10-27 18:20:16 +0100

    Polish figures for APSIPA.

--- profile_ism_order.py
+++ profile_ism_order.py
@@ -2,85 +2,18 @@ import click
 import time
 import pprint
 import numpy as np
+import pickle
 import matplotlib
 import matplotlib.pyplot as plt
 
 from utils import compute_room_irs, RoomSimSoftware
 
-font = {"family": "Helvetica", "weight": "normal", "size": 18}
+font = {"family": "Times New Roman", "weight": "normal", "size": 18}
 matplotlib.rc("font", **font)
 
 
-@click.command()
-@click.argument("n_trials", type=int, default=100)
-def profile_room_gen(n_trials):
-    print(
-        "\nCOMPARING ROOM SIMULATION SOFTWARE WITH {} TRIALS\n".format(
-            n_trials
-        )
-    )
-
-    software = [
-        RoomSimSoftware.PYGSOUND,
-        RoomSimSoftware.PYROOMACOUSTICS,
-    ]
-
+def plot_results(software, ism_order_vals, proc_time, proc_time_std, n_std):
     markers = ["o", "^", "v", "x", ">", "<", "D", "+"]
-    n_std = 1
-
-    # sweep number of rays
-    ism_order_vals = [2, 3, 4, 5, 6, 7]
-    n_rays = int(1e4)
-    proc_time = dict()
-    proc_time_std = dict()
-    for ism_order in ism_order_vals:
-
-        print("ISM order : {}".format(ism_order))
-        proc_time[ism_order] = dict()
-        proc_time_std[ism_order] = dict()
-
-        # loop through software
-        for _software in software:
-            ray_tracing_param = None
-
-            if _software == RoomSimSoftware.PYGSOUND:
-                ray_tracing_param = {
-                    "diffuse_count": int(n_rays),
-                    "specular_count": 6 ** ism_order,
-                    "specular_depth": ism_order,
-                }
-            elif _software == RoomSimSoftware.PYROOMACOUSTICS:
-                ray_tracing_param = {"n_rays": int(n_rays)}
-            assert ray_tracing_param is not None
-
-            timing = []
-            for _ in range(n_trials):
-                start_time = time.time()
-                compute_room_irs(
-                    room_dim=[8, 9, 3],
-                    room_properties=0.5,  # rt60
-                    scattering=0.5,
-                    ism_order=ism_order
-                    if _software == RoomSimSoftware.PYROOMACOUSTICS
-                    else None,
-                    mic_pos=[0.3, 3, 0.2],
-                    source_pos=[[3.2, 3, 1.8]],
-                    software=_software,
-                    ray_tracing_param=ray_tracing_param,
-                )
-                timing.append(time.time() - start_time)
-            proc_time[ism_order][_software] = np.mean(timing)
-            proc_time_std[ism_order][_software] = np.std(timing)
-            print(
-                "{} : {} seconds".format(
-                    _software, proc_time[ism_order][_software]
-                )
-            )
-
-    pprint.pprint(proc_time)
-    pprint.pprint(proc_time_std)
-
-    # plot results
     plt.figure()
     for i, _software in enumerate(software):
         _proc_time = []
@@ -107,12 +40,120 @@ def profile_room_gen(n_trials):
             alpha=0.2,
         )
 
-    plt.legend()
+    plt.legend(loc="upper left")
     plt.xlabel("Specular depth / ISM order")
-    plt.ylabel("Processing time [s]")
+    plt.ylabel("Processing time (s)")
     plt.grid()
     plt.tight_layout()
+    ax = plt.gca()
+    ax.set_xticks(ism_order_vals)
     plt.savefig("ism.png")
+
+
+@click.command()
+@click.option("--n_trials", type=int, default=100)
+@click.option("--pickle_path", type=str, default=None)
+def profile_room_gen(n_trials, pickle_path):
+    """
+    Parameters
+    ----------
+    n_trials : int
+        How many trials to average over. Ignored if `pickle_path` is provided.
+    pickle_path : str
+        File path to already computed results in order to plot them.
+    """
+
+    if pickle_path is None:
+        print(
+            "\nCOMPARING ROOM SIMULATION SOFTWARE WITH {} TRIALS\n".format(
+                n_trials
+            )
+        )
+
+        software = [
+            RoomSimSoftware.PYGSOUND,
+            RoomSimSoftware.PYROOMACOUSTICS,
+        ]
+        n_std = 1
+
+        # sweep number of rays
+        ism_order_vals = [2, 3, 4, 5, 6, 7]
+        n_rays = int(1e4)
+        proc_time = dict()
+        proc_time_std = dict()
+        for ism_order in ism_order_vals:
+
+            print("ISM order : {}".format(ism_order))
+            proc_time[ism_order] = dict()
+            proc_time_std[ism_order] = dict()
+
+            # loop through software
+            for _software in software:
+                ray_tracing_param = None
+
+                if _software == RoomSimSoftware.PYGSOUND:
+                    ray_tracing_param = {
+                        "diffuse_count": int(n_rays),
+                        "specular_count": 6 ** ism_order,
+                        "specular_depth": ism_order,
+                    }
+                elif _software == RoomSimSoftware.PYROOMACOUSTICS:
+                    ray_tracing_param = {"n_rays": int(n_rays)}
+                assert ray_tracing_param is not None
+
+                timing = []
+                for _ in range(n_trials):
+                    start_time = time.time()
+                    compute_room_irs(
+                        room_dim=[8, 9, 3],
+                        room_properties=0.5,  # rt60
+                        scattering=0.5,
+                        ism_order=ism_order
+                        if _software == RoomSimSoftware.PYROOMACOUSTICS
+                        else None,
+                        mic_pos=[0.3, 3, 0.2],
+                        source_pos=[[3.2, 3, 1.8]],
+                        software=_software,
+                        ray_tracing_param=ray_tracing_param,
+                    )
+                    timing.append(time.time() - start_time)
+                proc_time[ism_order][_software] = np.mean(timing)
+                proc_time_std[ism_order][_software] = np.std(timing)
+                print(
+                    "{} : {} seconds".format(
+                        _software, proc_time[ism_order][_software]
+                    )
+                )
+
+        pprint.pprint(proc_time)
+        pprint.pprint(proc_time_std)
+
+        data = {
+            "proc_time": proc_time,
+            "proc_time_std": proc_time_std,
+            "ism_order_vals": ism_order_vals,
+            "software": software,
+            "n_std": n_std,
+            "n_rays": n_rays,
+            "n_trials": n_trials,
+        }
+
+        with open("profile_ism_order.pickle", "wb") as handle:
+            pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    else:
+
+        with open(pickle_path, "rb") as f:
+            data = pickle.load(f)
+
+        software = data["software"]
+        ism_order_vals = data["ism_order_vals"]
+        proc_time = data["proc_time"]
+        proc_time_std = data["proc_time_std"]
+        n_std = data["n_std"]
+
+    # plot results
+    plot_results(software, ism_order_vals, proc_time, proc_time_std, n_std)
 
 
 if __name__ == "__main__":
--- profile_nrays.py
+++ profile_nrays.py
@@ -4,84 +4,16 @@ import numpy as np
 import pprint
 import matplotlib
 import matplotlib.pyplot as plt
+import pickle
 
 from utils import compute_room_irs, RoomSimSoftware
 
-font = {"family": "Helvetica", "weight": "normal", "size": 18}
+font = {"family": "Times New Roman", "weight": "normal", "size": 18}
 matplotlib.rc("font", **font)
 
 
-@click.command()
-@click.argument("n_trials", type=int, default=100)
-def profile_room_gen(n_trials):
-    print(
-        "\nCOMPARING ROOM SIMULATION SOFTWARE WITH {} TRIALS\n".format(
-            n_trials
-        )
-    )
-
-    software = [
-        RoomSimSoftware.PYGSOUND,
-        RoomSimSoftware.PYROOMACOUSTICS,
-    ]
-    n_std = 1
-
+def plot_results(software, n_ray_vals, proc_time, proc_time_std, n_std):
     markers = ["o", "^", "v", "x", ">", "<", "D", "+"]
-
-    # sweep number of rays
-    n_ray_vals = np.array([1e3, 3e3, 1e4, 3e4, 1e5], dtype=int)
-    ism_order = 3
-    proc_time = dict()
-    proc_time_std = dict()
-    for n_rays in n_ray_vals:
-
-        print("number of rays : {}".format(n_rays))
-        proc_time[n_rays] = dict()
-        proc_time_std[n_rays] = dict()
-
-        # loop through software
-        for _software in software:
-            ray_tracing_param = None
-
-            if _software == RoomSimSoftware.PYGSOUND:
-                ray_tracing_param = {
-                    "diffuse_count": int(n_rays),
-                    "specular_count": 6 ** ism_order,
-                    "specular_depth": ism_order,
-                }
-            elif _software == RoomSimSoftware.PYROOMACOUSTICS:
-                ray_tracing_param = {"n_rays": int(n_rays)}
-            assert ray_tracing_param is not None
-
-            timing = []
-            for _ in range(n_trials):
-                start_time = time.time()
-                compute_room_irs(
-                    room_dim=[8, 9, 3],
-                    room_properties=0.5,  # rt60
-                    scattering=0.5,
-                    ism_order=ism_order
-                    if _software == RoomSimSoftware.PYROOMACOUSTICS
-                    else None,
-                    mic_pos=[0.3, 3, 0.2],
-                    source_pos=[[3.2, 3, 1.8]],
-                    software=_software,
-                    ray_tracing=True,
-                    ray_tracing_param=ray_tracing_param,
-                )
-                timing.append(time.time() - start_time)
-            proc_time[n_rays][_software] = np.mean(timing)
-            proc_time_std[n_rays][_software] = np.std(timing)
-            print(
-                "{} : {} seconds".format(
-                    _software, proc_time[n_rays][_software]
-                )
-            )
-
-    pprint.pprint(proc_time)
-    pprint.pprint(proc_time_std)
-
-    # plot results
     plt.figure()
     for i, _software in enumerate(software):
         _proc_time = []
@@ -103,10 +35,121 @@ def profile_room_gen(n_trials):
 
     plt.legend()
     plt.xlabel("Number of rays")
-    plt.ylabel("Processing time [s]")
+    plt.ylabel("Processing time (s)")
     plt.grid()
+    plt.ylim([10 ** -2, 10 ** 1])
     plt.tight_layout()
+    ax = plt.gca()
+    ax.set_xticks(n_ray_vals)
     plt.savefig("number_rays.png")
+
+
+@click.command()
+@click.option("--n_trials", type=int, default=100)
+@click.option("--pickle_path", type=str, default=None)
+def profile_room_gen(n_trials, pickle_path):
+    """
+    Parameters
+    ----------
+    n_trials : int
+        How many trials to average over. Ignored if `pickle_path` is provided.
+    pickle_path : str
+        File path to already computed results in order to plot them.
+    """
+
+    if pickle_path is None:
+
+        print(
+            "\nCOMPARING ROOM SIMULATION SOFTWARE WITH {} TRIALS\n".format(
+                n_trials
+            )
+        )
+
+        software = [
+            RoomSimSoftware.PYGSOUND,
+            RoomSimSoftware.PYROOMACOUSTICS,
+        ]
+        n_std = 1
+
+        # sweep number of rays
+        n_ray_vals = np.array([1e3, 3e3, 1e4, 3e4, 1e5], dtype=int)
+        ism_order = 3
+        proc_time = dict()
+        proc_time_std = dict()
+        for n_rays in n_ray_vals:
+
+            print("number of rays : {}".format(n_rays))
+            proc_time[n_rays] = dict()
+            proc_time_std[n_rays] = dict()
+
+            # loop through software
+            for _software in software:
+                ray_tracing_param = None
+
+                if _software == RoomSimSoftware.PYGSOUND:
+                    ray_tracing_param = {
+                        "diffuse_count": int(n_rays),
+                        "specular_count": 6 ** ism_order,
+                        "specular_depth": ism_order,
+                    }
+                elif _software == RoomSimSoftware.PYROOMACOUSTICS:
+                    ray_tracing_param = {"n_rays": int(n_rays)}
+                assert ray_tracing_param is not None
+
+                timing = []
+                for _ in range(n_trials):
+                    start_time = time.time()
+                    compute_room_irs(
+                        room_dim=[8, 9, 3],
+                        room_properties=0.5,  # rt60
+                        scattering=0.5,
+                        ism_order=ism_order
+                        if _software == RoomSimSoftware.PYROOMACOUSTICS
+                        else None,
+                        mic_pos=[0.3, 3, 0.2],
+                        source_pos=[[3.2, 3, 1.8]],
+                        software=_software,
+                        ray_tracing=True,
+                        ray_tracing_param=ray_tracing_param,
+                    )
+                    timing.append(time.time() - start_time)
+                proc_time[n_rays][_software] = np.mean(timing)
+                proc_time_std[n_rays][_software] = np.std(timing)
+                print(
+                    "{} : {} seconds".format(
+                        _software, proc_time[n_rays][_software]
+                    )
+                )
+
+        pprint.pprint(proc_time)
+        pprint.pprint(proc_time_std)
+
+        data = {
+            "proc_time": proc_time,
+            "proc_time_std": proc_time_std,
+            "n_ray_vals": n_ray_vals,
+            "software": software,
+            "n_std": n_std,
+            "ism_order": ism_order,
+            "n_trials": n_trials,
+        }
+
+        with open("profile_nrays.pickle", "wb") as handle:
+            pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    else:
+
+        with open(pickle_path, "rb") as f:
+            data = pickle.load(f)
+
+        software = data["software"]
+        n_ray_vals = data["n_ray_vals"]
+        proc_time = data["proc_time"]
+        proc_time_std = data["proc_time_std"]
+        n_std = data["n_std"]
+
+    # plot results
+    plot_results(software, n_ray_vals, proc_time, proc_time_std, n_std)
 
 
 if __name__ == "__main__":
